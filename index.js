525be973dad4c2b4cb54e77dee64e2ac47a20e75
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-09-30 10:58:05 +0600

    add pagination

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb://localhost:27017" 
+// const uri = "mongodb://localhost:27017" 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqhfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqhfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +30,28 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // all collection
+        const productCollection = client.db("shopDb").collection("products");
+
+        // products related apis
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const result = await productCollection.find()
+            .skip(page * size)
+            .limit(size)
+            .toArray();
+            res.send(result);
+        })
+
+        // get products count
+        app.get('/productsCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
